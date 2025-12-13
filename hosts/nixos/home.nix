2bd941b124e02f131d110c31fe88bdeb650cb90b
Alexandre <mateialex@outlook.fr>
2025-12-14 00:10:26 +0100

    Removed the pkgs. prefix, added with pkgs option in homeManager in NixOS

--- hosts/nixos/home.nix
+++ hosts/nixos/home.nix
@@ -54,17 +54,14 @@ commonHome // {
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
-    pkgs.hello
-    pkgs.btop
-    pkgs.fastfetch
 
     # Hyprland related (rice, etc...)
-    pkgs.wofi
-    pkgs.alacritty
-    pkgs.firefox
+    wofi
+    alacritty
+    firefox
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
