d48c0cefb278b56e7fbef960bda9d18878b956fc
Alexandre <mateialex@outlook.fr>
2025-12-14 19:26:27 +0100

    Fixed the SSH and SDDM issue

--- modules/common/ssh.nix
+++ modules/common/ssh.nix
@@ -2,5 +2,10 @@
   # The OpenSSH agent
   programs.ssh = {
     startAgent = true;
+    extraConfig = ''
+      # Test if github.com works with ssh for cloning
+      Host github.com
+      IdentityFile ~/.ssh/github
+    '';
   };
 }
