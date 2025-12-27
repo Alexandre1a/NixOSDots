a9170a1eca758bb138dafd3c72c9b9754952e22f
Alexandre <mateialex@outlook.fr>
2025-12-27 02:10:48 +0100

    Added secrets and modified Nvidia settings

--- modules/nixos/gamming/nvidia.nix
+++ modules/nixos/gamming/nvidia.nix
@@ -7,11 +7,42 @@
     };
     #Nvidia Drivers
     nvidia = {     
-      open = true;
+      open = false; # My 1660S works better with closed source drivers 
       modesetting = {
         enable = true;
       };
     };
+  };
+
+  boot = {
+    kernelParams = [
+      "nvidia-drm.modset=1" # DRM modesetting
+    ];
+    initrd.kernelModules = [ 
+      "nvidia" 
+      "nvidia_modeset" 
+      "nvidia_uvm" 
+      "nvidia_drm" 
+    ];
+  };
+
+  # Environment variables
+  environment.sessionVariables = {
+    # Backend GBM for NVIDIA
+    GBM_BACKEND = "nvidia-drm";
+    
+    # Use NVIDIA pilot for libva (video acceleration)
+    LIBVA_DRIVER_NAME = "nvidia";
+    
+    # Vendor library OpenGL
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    
+    XDG_SESSION_TYPE = "wayland";
+    
+    WLR_NO_HARDWARE_CURSORS = "1";
+    
+    # Native Wayland Electron Apps
+    NIXOS_OZONE_WL = "1";
   };
 
   # Nvidia Drivers
