9c757456d0d2f6ee9a479f5546c0c285c7894175
Alexandre <mateialex@outlook.fr>
2025-12-27 04:03:26 +0100

    Fixed a missing import

--- hosts/nixos/modules.nix
+++ hosts/nixos/modules.nix
@@ -5,6 +5,7 @@
   imports = 
     [
       inputs.sops-nix.nixosModules.sops
+      ../../modules/nixos/secrets.nix # Sops
       ../../modules/nixos/wireless.nix # For the wifi
       ../../modules/nixos/locale.nix # For localisation options
       ../../modules/nixos/gamming/nvidia.nix # Nvidia (Drivers and Settings)
