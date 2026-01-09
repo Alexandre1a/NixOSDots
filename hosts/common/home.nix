7b53ac0d7e2adeaebbb52ffcf707dbe75062a358
Alexandre1a <Mateialex@outlook.fr>
2026-01-10 00:10:56 +0100

    Removded ZED for system stability

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -5,7 +5,7 @@
   imports = [
     inputs.spicetify-nix.homeManagerModules.spicetify
     ../../modules/home-manager/spicetify.nix
-    ../../modules/home-manager/zed.nix
+    #../../modules/home-manager/zed.nix
     ../../modules/home-manager/shell.nix
   ];
 
