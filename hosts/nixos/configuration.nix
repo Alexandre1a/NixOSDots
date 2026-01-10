fb98d7f0ecc638862b284d6fa24b7320c3ddef59
Alexandre <mateialex@outlook.fr>
2026-01-10 02:37:47 +0100

    Added a limit for boot entries to try to reduce /boot usage

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -12,8 +12,17 @@
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot = {
+        enable = true;
+	configurationLimit = 2;
+      };
+      efi = {
+        canTouchEfiVariables = true;
+      };
+    };
+  };
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
