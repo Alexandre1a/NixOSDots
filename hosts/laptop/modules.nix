9d5a736a79448ddad8c016f53f666fdd51b28b47
Alexandre1a <Mateialex@outlook.fr>
2026-01-25 21:30:55 +0100

    Fixed path for laptop config

--- hosts/laptop/modules.nix
+++ hosts/laptop/modules.nix
@@ -2,8 +2,8 @@
   imports = [
     ../common/modules.nix
     ./hardware-configuration.nix
-    ../../modules/nixos/gamming/steam.nix
-    ../../modules/nixos/gamming/nvidia.nix
+    ../../modules/nixos/gaming/steam.nix
+    ../../modules/nixos/gaming/nvidia.nix
   ];
 
 }
