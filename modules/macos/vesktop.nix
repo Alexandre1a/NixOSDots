edff467449141f25b99a9adcdb518e677da1d6c7
Alexandre1a <Mateialex@outlook.fr>
2026-01-31 19:37:01 +0100

    Modified the overlay to use binaries

--- modules/macos/vesktop.nix
+++ modules/macos/vesktop.nix
@@ -1,13 +1,25 @@
-{ config, pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   nixpkgs.overlays = [
     (final: prev: {
-      vesktop = prev.vesktop.overrideAttrs (old: {
-        env = (old.env or {}) // {
-          ELECTRON_BUILDER_DISABLE_SIGNING = "true";
+      vesktop = prev.vesktop.overrideAttrs (_: {
+        dontBuild = true;
+        dontConfigure = true;
+
+        src = pkgs.fetchzip {
+          url = "https://github.com/Vencord/Vesktop/releases/download/v1.6.4/Vesktop-1.6.4-universal-mac.zip";
+          sha256 = "a78d0abcecfb1c615b7bd3ebe0428b4ae9f8a6f6ffa6dd288bb42f69a440dfa";
         };
+
+        installPhase = ''
+          mkdir -p $out/Applications
+          cp -R Vesktop.app $out/Applications/
+        '';
+
+        meta.platforms = [ "aarch64-darwin" ];
       });
     })
   ];
 }
+
