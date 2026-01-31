37769da851d1188723b2288299cf5d0e9e874d14
Alexandre1a <Mateialex@outlook.fr>
2026-01-31 16:28:14 +0100

    Added Direnv and removed AIs from macos

--- hosts/common/modules.nix
+++ hosts/common/modules.nix
@@ -2,6 +2,7 @@
     # Common nixos systems modules (not nix-darwin)
     imports = [
       ../../modules/common/nix.nix
+      ../../modules/common/direnv.nix
       ../../modules/nixos/swap.nix
       ../../modules/nixos/locale.nix # For localisation options
       ../../modules/common/ssh.nix
--- hosts/macos/modules.nix
+++ hosts/macos/modules.nix
@@ -4,7 +4,7 @@
 # Enpty file for future imports
   imports = [
     ../../modules/common/nix.nix
-    ../../modules/macos/ollama.nix
-    ../../modules/common/ai.nix
+    #../../modules/macos/ollama.nix
+    ../../modules/common/direnv.nix
   ];
 }
